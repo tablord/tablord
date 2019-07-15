1bc7a19dd0299369888fde660ded0d9c1bb7a294
marcnicole <marc9nicole@gmail.com>
2019-07-15 08:49:29 +0200

    test of fail

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.equal([1, 2, 3,4].indexOf(4), -1);
     });
   });
 });
