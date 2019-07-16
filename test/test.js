1f10d614d8347200a8e0c0a0fbc0273d967af88b
marcnicole <marc9nicole@gmail.com>
2019-07-16 14:09:52 +0200

    just started to do some real test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
 var assert = require('assert');
+var helper = require('../src/helper.js');
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1); // should be ok
     });
   });
+});
+
+describe('helper', function() {
+  describe('#tb.reduce.sum()', function() {
+    if('should return the sum of two numbers', function() {
+      assert.equal(tb.reduce.sum(2,3),5);
+    })
+  })
 });
